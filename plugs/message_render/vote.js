295e5e4e33bfb464d2d6686f8eeb65b0c9451882
Matt McKegg <matt@wetsand.co.nz>
2017-02-13 04:46:11 +1300

    add vote message_renderer

---
+++ plugs/message_render/vote.js
@@ -0,0 +1,32 @@
+var h = require('mutant/h')
+
+exports.needs = {
+  message_layout: 'first',
+  message_link: 'first',
+  markdown: 'first'
+}
+
+exports.gives = {
+  message_render: true
+}
+
+exports.create = function (api) {
+  return {
+    message_render
+  }
+
+  function message_render (msg) {
+    if (msg.value.content.type !== 'vote') return
+    return api.message_layout(msg, {
+      content: render_vote(msg),
+      mini: true
+    })
+  }
+
+  function render_vote (msg) {
+    var link = msg.value.content.vote.link
+    return [
+      msg.value.content.vote.value > 0 ? 'dug' : 'undug', ' ', api.message_link(link)
+    ]
+  }
+}
