adf8a7771064a6d9a18b6005e625fadbd6fb9846
Matt McKegg <matt@wetsand.co.nz>
2017-02-14 01:27:00 +1300

    use patchwork-next version of message_name

--- components/message/name.js
+++ components/message/name.js
@@ -1,25 +1,33 @@
-
-function title (s) {
-  var m = /^\n*([^\n]{0,40})/.exec(s)
-  return m && (m[1].length == 40 ? m[1]+'...' : m[1])
-}
-
 exports.needs = { sbot_get: 'first' }
 exports.gives = 'message_name'
 
-//TODO: rewrite as observable?
+// needs an async version
 
 exports.create = function (api) {
   return function (id, cb) {
     api.sbot_get(id, function (err, value) {
-      if(err && err.name == 'NotFoundError')
-        return cb(null, id.substring(0, 10)+'...(missing)')
-      if(value.content.type === 'post' && 'string' === typeof value.content.text)
-        return cb(null, title(value.content.text))
-      else if('string' === typeof value.content.text)
-        return cb(null, value.content.type + ':'+title(value.content.text))
-      else
-        return cb(null, id.substring(0, 10)+'...')
+      if (err && err.name === 'NotFoundError') {
+        return cb(null, id.substring(0, 10) + '...(missing)')
+      } else if (value.content.type === 'post' && typeof value.content.text === 'string') {
+        if (value.content.text.trim()) {
+          return cb(null, titleFromMarkdown(value.content.text, 40))
+        }
+      } else if (typeof value.content.text === 'string') {
+        return cb(null, value.content.type + ': ' + titleFromMarkdown(value.content.text, 30))
+      }
+
+      return cb(null, id.substring(0, 10) + '...')
     })
   }
+}
+
+function titleFromMarkdown (text, max) {
+  text = text.trim().split('\n', 2).join('\n')
+  text = text.replace(/_|`|\*|\#|\[.*?\]|\(\S*?\)/g, '').trim()
+  text = text.replace(/\:$/, '')
+  text = text.trim().split('\n', 1)[0].trim()
+  if (text.length > max) {
+    text = text.substring(0, max - 2) + '...'
+  }
+  return text
 }
