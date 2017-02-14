85adb6e5580e738c24e40aa463d345bc5bc473e3
Matt McKegg <matt@wetsand.co.nz>
2017-02-14 17:49:42 +1300

    turn channel meta into link

--- message/html/meta/channel.js
+++ message/html/meta/channel.js
@@ -6,6 +6,6 @@ exports.gives = nest('message.html.meta')
 exports.create = (api) => {
   return nest('message.html.meta', function channel (msg) {
     const { channel } = msg.value.content
-    if (channel) return h('span', {}, ['#' + channel])
+    if (channel) return h('a.channel', {href: `#${channel}`}, [`#${channel}`])
   })
 }
