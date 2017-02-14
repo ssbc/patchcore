92df89e8645007b7ed21d15d496db762a37d4f49
Matt McKegg <matt@wetsand.co.nz>
2017-02-14 15:56:53 +1300

    keys: fix caching, add keys.sync.id

--- keys.js
+++ keys.js
@@ -3,15 +3,27 @@ const Keys = require('ssb-keys')
 const nest = require('depnest')
 
 exports.needs = nest('config.sync.load', 'first')
-exports.gives = nest('keys.sync.load')
+exports.gives = nest({
+  'keys.sync': [ 'load', 'id' ]
+})
+
 exports.create = (api) => {
-  return nest('keys.sync.load', () => {
-    var keys
+  var keys
+
+  return nest({
+    'keys.sync': { load, id }
+  })
+
+  function id () {
+    return load().id
+  }
+
+  function load () {
     if (!keys) {
       const config = api.config.sync.load()
       const keyPath = Path.join(config.path, 'secret')
       keys = Keys.loadOrCreateSync(keyPath)
     }
     return keys
-  })
+  }
 }
