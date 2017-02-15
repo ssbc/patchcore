36538d432d86562d5af0cf3fcb16c3937a8e8aa6
Matt McKegg <matt@wetsand.co.nz>
2017-02-16 01:10:48 +1300

    tweak message.async.name for stripping @ mention from start of message

--- message/async/name.js
+++ message/async/name.js
@@ -7,25 +7,26 @@ exports.gives = nest('message.async.name')
 
 exports.create = function (api) {
   return nest('message.async.name', function (id, cb) {
+    var fallbackName = id.substring(0, 10) + '...'
     api.sbot.async.get(id, function (err, value) {
       if (err && err.name === 'NotFoundError') {
-        return cb(null, id.substring(0, 10) + '...(missing)')
+        return cb(null, fallbackName + '...(missing)')
       } else if (value.content.type === 'post' && typeof value.content.text === 'string') {
         if (value.content.text.trim()) {
-          return cb(null, titleFromMarkdown(value.content.text, 40))
+          return cb(null, titleFromMarkdown(value.content.text, 40) || fallbackName)
         }
       } else if (typeof value.content.text === 'string') {
         return cb(null, value.content.type + ': ' + titleFromMarkdown(value.content.text, 30))
       }
 
-      return cb(null, id.substring(0, 10) + '...')
+      return cb(null, fallbackName)
     })
   })
 }
 
 function titleFromMarkdown (text, max) {
-  text = text.trim().split('\n', 2).join('\n')
-  text = text.replace(/_|`|\*|\#|\[.*?\]|\(\S*?\)/g, '').trim()
+  text = text.trim().split('\n', 3).join('\n')
+  text = text.replace(/_|`|\*|\#|^\[@.*?\]|\[|\]|\(\S*?\)/g, '').trim()
   text = text.replace(/\:$/, '')
   text = text.trim().split('\n', 1)[0].trim()
   if (text.length > max) {
