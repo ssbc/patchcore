b7c9439125a528967ba78bac1f8d7c0372e3aec6
Matt McKegg <matt@wetsand.co.nz>
2017-02-16 17:54:13 +1300

    private feed: better handling of limit

--- feed/pull/private.js
+++ feed/pull/private.js
@@ -1,5 +1,6 @@
 const pull = require('pull-stream')
 const nest = require('depnest')
+const extend = require('xtend')
 
 exports.gives = nest('feed.pull.private')
 exports.needs = nest({
@@ -9,10 +10,25 @@ exports.needs = nest({
 
 exports.create = function (api) {
   return nest('feed.pull.private', function (opts) {
-    return pull(
+    var opts = extend(opts)
+
+    // handle limit to ensure we're getting old private messages
+    var limit = opts.limit
+    delete opts.limit
+
+    var stream = pull(
       api.sbot.pull.log(opts),
       unbox()
     )
+
+    if (limit) {
+      return pull(
+        stream,
+        pull.take(limit)
+      )
+    } else {
+      return stream
+    }
   })
 
   // scoped
