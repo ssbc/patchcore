0cf9452d70be8af1d22ef550fc9602276f10334d
Matt McKegg <matt@wetsand.co.nz>
2017-02-18 02:10:18 +1300

    wrap main depject exports with "patchcore"

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const bulk = require('bulk-require')
-module.exports = bulk(__dirname, [
-  './+(config|keys|sbot|emoji).js',
-  './+(lib|about|feed|message|contact|blob)/**/*.js'
-])
+module.exports = {
+  patchcore: bulk(__dirname, [
+    './+(config|keys|sbot|emoji).js',
+    './+(lib|about|feed|message|contact|blob)/**/*.js'
+  ])
+}
