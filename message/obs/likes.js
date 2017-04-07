bf11fb1415cc7ffb6d5a37e90c86dc8ca73f482d
mix irving <whimful@gmail.com>
2017-04-08 00:04:37 +1200

    obs like refactor (#5)

--- message/obs/likes.js
+++ message/obs/likes.js
@@ -15,20 +15,21 @@ exports.create = function (api) {
   var likesLookup = {}
   return nest({
     'sbot.hook.feed': (msg) => {
-      if (msg.value && typeof msg.value.content === 'string') {
+      if (!(msg && msg.value && msg.value.content)) return
+      if (typeof msg.value.content === 'string') {
         msg = api.message.sync.unbox(msg)
+        if (!msg) return
       }
-      if (msg && msg.value && msg.value.content) {
-        var c = msg.value.content
-        if (c.type === 'vote') {
-          if (msg.value.content.vote && msg.value.content.vote.link) {
-            var likes = get(msg.value.content.vote.link)()
-            if (!likes[msg.value.author] || likes[msg.value.author][1] < msg.timestamp) {
-              likes[msg.value.author] = [msg.value.content.vote.value > 0, msg.timestamp]
-              get(msg.value.content.vote.link).set(likes)
-            }
-          }
-        }
+
+      var c = msg.value.content
+      if (c.type !== 'vote') return
+      if (!c.vote || !c.vote.link) return 
+
+      var likes = get(c.vote.link)()
+      var author = msg.value.author
+      if (!likes[author] || likes[author][1] < msg.timestamp) {
+        likes[author] = [c.vote.value > 0, msg.timestamp]
+        get(c.vote.link).set(likes)
       }
     },
     'message.obs.likes': (id) => {
