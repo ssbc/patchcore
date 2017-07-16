67568ff7c705f4f8538755e6f3682678d0859822
mix irving <whimful@gmail.com>
2017-07-16 22:35:06 +1200

    switch to object-based routing

--- router/sync/router.js
+++ router/sync/router.js
@@ -1,5 +1,5 @@
 const nest = require('depnest')
-const sheetRouter = require('sheet-router')
+const { isBlob, isFeed, isMsg } = require('ssb-ref')
 
 exports.gives = nest('router.sync.router')
 
@@ -7,16 +7,37 @@ exports.needs = nest('router.sync.routes', 'reduce')
 
 exports.create = (api) => {
   var _router = null
-  return nest('router.sync.router', router)
 
-  function router (path) {
-    if (_router) return _router(path)
+  return nest('router.sync.router', (location) => {
+    if (!_router) {
+      const routes = api.router.sync.routes()
+      _router = buildRouter(routes)
+    }
 
-    _router = sheetRouter(
-      {default: '/'},
-      api.router.sync.routes()
-    )
-    return _router(path)
+    const locationObject = normalise(location)
+    return _router(locationObject)
+  })
+}
+
+function normalise(location) {
+  if (typeof location === 'object') return location
+
+  if (isBlob(location)) return { blob: location }
+  if (isChannel(location)) return { channel: location }
+  if (isFeed(location)) return { feed: location }
+  if (isMsg(location)) return { msg: location }
+}
+
+function isChannel (str) {
+  return typeof str === 'string' && str[0] === '#' && str.length > 1
+}
+
+function buildRouter (routes) {
+  return (location) => {
+    const route = routes.find(([validator]) => validator(location))
+    // signature of a route is [ routeValidator, routeFunction ]
+
+    if (route) return route[1](location)
   }
 }
 
