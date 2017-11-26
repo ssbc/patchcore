e6ada2f6daafcd03bde943759ecec74629525fb6
Gordon Martin <gordon.hugh.martin@gmail.com>
2017-11-26 22:27:25 +0000

    Changed the 'gives' name to fit the depject patchcore conventions matt mentioned. Also changed the second argument to accept 'opts' to make the space to also add an optional 'object' to filter on directly within the sbot query. Remove the 'sync' observable as mutant-pull-reduce is meant to deal with that.

--- backlinks/obs_filter.js
+++ backlinks/obs_filter.js
@@ -7,16 +7,16 @@ exports.needs = nest({
   'sbot.pull.backlinks': 'first'
 })
 
-exports.gives = nest("backlinks.filter.obs", true)
+exports.gives = nest("backlinks.obs.filter", true)
 
 /**
  * sbot.filter.obs returns an observable list of messages that link
  * back to the message with the given message ID (@id). Only messages that
- * pass the filter function are added to the list.
+ * pass the filter are added to the list.
  *
- * When a message arrives, if passing it to the filter function (@filterFn)
- * does not result in it returning 'true' the message is not added to the
- * observable list.
+ * When a message arrives, if a filter function is given in the options (opts.filter)
+ * and passing it to the filter function (opts.filter) not result in it returning
+ * 'true' the message is not added to the observable list.
  *
  * A 'sync' observable property is also added to the returned observable
  * which is 'true' when all previously seen messages are caught up with.
@@ -25,40 +25,29 @@ exports.gives = nest("backlinks.filter.obs", true)
  * callers that supply the same arguments.
  */
 exports.create = function(api) {
-  function pullFilterReduceObs(id, filterFn) {
+  function pullFilterReduceObs(id, opts) {
     if (!id || typeof(id) !== "string") {
       throw new Error("id must be a string.")
     }
 
-    if (!filterFn || typeof(filterFn) !== "function") {
-      throw new Error("filterFn must be a function.")
+    var sbotFilter = {
+      $filter: {
+        dest: id
+      }
     }
 
-    var sync = Value(false)
-
     var msgBacklinks = api.sbot.pull.backlinks({
-      query: [{
-        $filter: {
-          dest: id
-        }
-      }],
+      query: [sbotFilter],
       live: true
     })
 
-    var msgFilter = pull.filter((msg) => {
-      // We do not include the 'sync' message which indicates that any
-      // further messages are newly gossiped messages in the list of messages.
-      if (msg.sync) {
-        sync.set(true)
-        return false
-      } else {
-        return filterFn(msg)
-      }
-    })
+    // If a filter function is supplied in the options, we use it to filter
+    // the links stream, otherwise we use all the messages from the stream
+    var filterFunction = opts && opts.filter ? opts.filter : () => true
 
     var filteredBacklinks = pull(
       msgBacklinks,
-      msgFilter
+      pull.filter(filterFunction)
     )
 
     var backlinksObs = MutantPullReduce(filteredBacklinks, (state, msg) => {
@@ -66,15 +55,14 @@ exports.create = function(api) {
       return state;
     }, {
       startValue: [],
-      nextTick: true
+      nextTick: true,
+      sync: true
     })
-
-    backlinksObs.sync = sync
 
     return backlinksObs;
   }
 
   return nest({
-    "backlinks.filter.obs": (id, filterFn) => pullFilterReduceObs(id, filterFn)
+    "backlinks.obs.filter": (id, opts) => pullFilterReduceObs(id, opts)
   })
 }
