b70e39416e0ec7a808742b9841a6cd32d45b3354
Gordon Martin <gordon.hugh.martin@gmail.com>
2017-11-26 19:22:27 +0000

    Add new backlinks filter obs function. This is a helper function to watch for links back to a message and add them to an observable list if they pass a given filter.

---
+++ backlinks/obs_filter.js
@@ -0,0 +1,80 @@
+var nest = require('depnest')
+var pull = require('pull-stream')
+var MutantPullReduce = require('mutant-pull-reduce')
+var Value = require('mutant/value')
+
+exports.needs = nest({
+  'sbot.pull.backlinks': 'first'
+})
+
+exports.gives = nest("backlinks.filter.obs", true)
+
+/**
+ * sbot.filter.obs returns an observable list of messages that link
+ * back to the message with the given message ID (@id). Only messages that
+ * pass the filter function are added to the list.
+ *
+ * When a message arrives, if passing it to the filter function (@filterFn)
+ * does not result in it returning 'true' the message is not added to the
+ * observable list.
+ *
+ * A 'sync' observable property is also added to the returned observable
+ * which is 'true' when all previously seen messages are caught up with.
+ *
+ * Note: Unlike backlinks.obs.for this does not cache the observable for
+ * callers that supply the same arguments.
+ */
+exports.create = function(api) {
+  function pullFilterReduceObs(id, filterFn) {
+    if (!id || typeof(id) !== "string") {
+      throw new Error("id must be a string.")
+    }
+
+    if (!filterFn || typeof(filterFn) !== "function") {
+      throw new Error("filterFn must be a function.")
+    }
+
+    var sync = Value(false)
+
+    var msgBacklinks = api.sbot.pull.backlinks({
+      query: [{
+        $filter: {
+          dest: id
+        }
+      }],
+      live: true
+    })
+
+    var filteredBacklinks = pull(
+      msgBacklinks,
+      pull(
+        pull.map(setSyncIfSyncMsg.bind(null, sync),
+          pull.filter(filterFn)
+        ))
+    )
+
+    var backlinksObs = MutantPullReduce(filteredBacklinks, (state, msg) => {
+      state.push(msg)
+      return state;
+    }, {
+      startValue: [],
+      nextTick: true
+    })
+
+    backlinksObs.sync = sync
+
+    return backlinksObs;
+  }
+
+  return nest({
+    "backlinks.filter.obs": (id, filterFn) => pullFilterReduceObs(id, filterFn)
+  })
+}
+
+function setSyncIfSyncMsg(syncObs, msg) {
+  if (msg.sync) {
+    syncObs.set(true)
+  }
+
+  return msg
+}
