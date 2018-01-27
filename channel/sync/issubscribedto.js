014e9fc877c2b3449f0e0f0d8328fa07674a50fa
andre alves garzia <andre@andregarzia.com>
2018-01-26 22:29:22 -0200

    feature: make channel.sync.isSubscribedTo more flexible

--- channel/sync/issubscribedto.js
+++ channel/sync/issubscribedto.js
@@ -11,11 +11,13 @@ exports.gives = nest('channel.sync.isSubscribedTo')
 exports.create = function (api) {
   return nest('channel.sync.isSubscribedTo', isSubscribedTo)
 
-  function isSubscribedTo (channel, cb) {
-    const myId = api.keys.sync.id()
+  function isSubscribedTo (channel, id) {
+    if (!ref.isFeed(id)) {
+      id = api.keys.sync.id()
+    }
         
     const { subscribed } = api.channel.obs
-    const myChannels = subscribed(myId)
+    const myChannels = subscribed(id)
     let v = myChannels().values()
     return [...v].includes(channel)
   }
