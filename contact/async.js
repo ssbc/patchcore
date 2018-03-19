d0f5c59e642c26f8ee4ecd895de7d71879513dab
Gordon Martin <gordon.hugh.martin@gmail.com>
2018-03-19 20:22:48 +0000

    When following a user, include a list of IDs of the user's name, description and image if your client knows it so that other people's clients can pull them in.

--- contact/async.js
+++ contact/async.js
@@ -1,10 +1,13 @@
 var nest = require('depnest')
 var ref = require('ssb-ref')
+var onceTrue = require('mutant/once-true')
+var About = require('ssb-ooo-about')
 
 exports.needs = nest({
   'contact.obs.following': 'first',
   'sbot.async.publish': 'first',
-  'sbot.async.friendsGet': 'first'
+  'sbot.async.friendsGet': 'first',
+  'sbot.obs.connection': 'first'
 })
 
 exports.gives = nest({
@@ -12,6 +15,7 @@ exports.gives = nest({
 })
 
 exports.create = function (api) {
+
   return nest({
     'contact.async': {follow, unfollow, followerOf, block, unblock}
   })
@@ -22,11 +26,34 @@ exports.create = function (api) {
 
   function follow (id, cb) {
     if (!ref.isFeed(id)) throw new Error('a feed id must be specified')
-    api.sbot.async.publish({
-      type: 'contact',
-      contact: id,
-      following: true
-    }, cb)
+
+    onceTrue(api.sbot.obs.connection, sbot => {
+      var aboutMessages = About(sbot, {});
+
+      aboutMessages.async.getLatestMsgIds( id, (err, aboutMessageIds) => {
+
+        if (err) {
+          cb(err, null)
+        } else {
+          
+          var followMsg = {
+            type: 'contact',
+            contact: id,
+            following: true
+          }
+
+          // If the user who is following the user is aware of their assigned
+          // name, description and image, we add those as a 'branch' field so
+          // a client can see parts of their profile by using ssb-ooo.
+          if (aboutMessageIds && aboutMessageIds.length > 0) {
+            followMsg['branch'] = aboutMessageIds;
+          }
+
+          api.sbot.async.publish(followMsg, cb)
+        }
+      })
+
+    })
   }
 
   function unfollow (id, cb) {
