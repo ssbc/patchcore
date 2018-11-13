80369cab6d5a5fdedd7a4140366209f03fd9656e
mixmix <whimful@gmail.com>
2018-11-13 19:11:48 +1300

    refactor to use ssb-about@2

--- about/obs.js
+++ about/obs.js
@@ -1,9 +1,10 @@
-var {Value, computed} = require('mutant')
-var pull = require('pull-stream')
+var { computed } = require('mutant')
 var nest = require('depnest')
 var ref = require('ssb-ref')
 var colorHash = new (require('color-hash'))()
 var fallbackImageUrl = 'data:image/gif;base64,R0lGODlhAQABAPAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='
+var MutantPullValue = require('../lib/mutant-pull-value')
+var MutantPullDict = require('../lib/mutant-pull-dict')
 
 exports.needs = nest({
   'sbot.pull.stream': 'first',
@@ -24,15 +25,13 @@ exports.gives = nest({
     'latestValue',
     'valueFrom',
     'socialValue',
-    'groupedValues'
+    'groupedValues',
+    'socialValues'
   ]
 })
 
 exports.create = function (api) {
-  var syncValue = Value(false)
-  var sync = computed(syncValue, x => x)
-  var cache = null
-
+  var socialValueCache = {}
   return nest({
     'about.obs': {
       // quick helpers, probably should deprecate!
@@ -42,101 +41,69 @@ exports.create = function (api) {
       names: (id) => groupedValues(id, 'name'),
       images: (id) => groupedValues(id, 'image'),
       color: (id) => computed(id, (id) => colorHash.hex(id)),
-      imageUrl: (id) => computed(socialValue(id, 'image'), (blobId) => {
-        return blobId ? api.blob.sync.url(blobId) : fallbackImageUrl
+      imageUrl: (id) => computed(socialValue(id, 'image'), (blobData) => {
+        if (!blobData) return fallbackImageUrl
+        const blobId = typeof blobData.link === 'string' ? blobData.link : blobData
+        return api.blob.sync.url(blobId)
       }),
 
       // custom abouts (the future!)
       valueFrom,
       latestValue,
       socialValue,
+      socialValues,
       groupedValues
     }
   })
 
   function valueFrom (id, key, author) {
     if (!ref.isLink(id)) throw new Error('About requires an ssb ref!')
-    return withSync(computed([get(id), key, author], getValueFrom))
+    return MutantPullValue(() => {
+      return api.sbot.pull.stream((sbot) => sbot.about.valueFromStream({ dest: id, key, id: author }))
+    })
   }
 
   function latestValue (id, key) {
     if (!ref.isLink(id)) throw new Error('About requires an ssb ref!')
-    return withSync(computed([get(id), key], getLatestValue))
+    return MutantPullValue(() => {
+      return api.sbot.pull.stream((sbot) => sbot.about.latestValueStream({ dest: id, key }))
+    })
   }
 
   function socialValue (id, key, defaultValue) {
     if (!ref.isLink(id)) throw new Error('About requires an ssb ref!')
-    var yourId = api.keys.sync.id()
-    return withSync(computed([get(id), key, id, yourId, defaultValue], getSocialValue))
+    if (!socialValueCache[id + '/' + key]) {
+      var obs = socialValueCache[id + '/' + key] = MutantPullValue(() => {
+        return api.sbot.pull.stream((sbot) => sbot.about.socialValueStream({ dest: id, key }))
+      }, {
+        onListen: () => { socialValueCache[id + '/' + key] = obs },
+        onUnlisten: () => delete socialValueCache[id + '/' + key]
+      })
+    }
+    return withDefault(socialValueCache[id + '/' + key], defaultValue)
+  }
+
+  function socialValues (id, key) {
+    if (!ref.isLink(id)) throw new Error('About requires an ssb ref!')
+    return MutantPullDict(() => {
+      return api.sbot.pull.stream((sbot) => sbot.about.socialValuesStream({ dest: id, key }))
+    }, { checkDelete })
   }
 
   function groupedValues (id, key) {
     if (!ref.isLink(id)) throw new Error('About requires an ssb ref!')
-    return withSync(computed([get(id), key], getGroupedValues))
-  }
-
-  function withSync (obs) {
-    obs.sync = sync
-    return obs
-  }
-
-  function get (id) {
-    if (!ref.isLink(id)) throw new Error('About requires an ssb ref!')
-    load()
-    if (!cache[id]) {
-      cache[id] = Value({})
-    }
-    return cache[id]
-  }
-
-  function load () {
-    if (!cache) {
-      cache = {}
-      pull(
-        api.sbot.pull.stream(sbot => sbot.about.stream({live: true})),
-        pull.drain(item => {
-          for (var target in item) {
-            var state = get(target)
-            var lastState = state()
-            var values = item[target]
-            var changed = false
-            for (var key in values) {
-              var valuesForKey = lastState[key] = lastState[key] || {}
-              for (var author in values[key]) {
-                var value = values[key][author]
-                if (!valuesForKey[author] || value[1] > valuesForKey[author][1]) {
-                  valuesForKey[author] = value
-                  changed = true
-                }
-              }
-            }
-            if (changed) {
-              state.set(lastState)
-            }
-          }
-
-          if (!syncValue()) {
-            syncValue.set(true)
-          }
-        })
-      )
-    }
+    return computed(socialValues(id, key), getGroupedValues)
   }
 }
 
-function getSocialValue (lookup, key, id, yourId, fallback) {
-  var result = lookup[key] ? getValue(lookup[key][yourId]) || getValue(lookup[key][id]) || highestRank(lookup[key]) : null
-  if (result != null) {
-    return result
-  } else {
-    return fallback || null
-  }
+function checkDelete (msg) {
+  if (msg && msg.remove) return true
 }
 
-function getGroupedValues (lookup, key) {
+function getGroupedValues (lookup) {
   var values = {}
-  for (var author in lookup[key]) {
-    var value = getValue(lookup[key][author])
+  for (var author in lookup) {
+    var value = getValue(lookup[author])
     if (value != null) {
       values[value] = values[value] || []
       values[value].push(author)
@@ -145,55 +112,26 @@ function getGroupedValues (lookup, key) {
   return values
 }
 
-function highestRank (lookup) {
-  var counts = {}
-  var highestCount = 0
-  var currentHighest = null
-  for (var key in lookup) {
-    var value = getValue(lookup[key])
-    if (value != null) {
-      counts[value] = (counts[value] || 0) + 1
-      if (counts[value] > highestCount) {
-        currentHighest = value
-        highestCount = counts[value]
-      }
-    }
-  }
-  return currentHighest
-}
-
 function getValue (item) {
-  if (item && item[0]) {
-    if (typeof item[0] === 'string') {
-      return item[0]
-    } else if (item[0] && item[0].link && ref.isLink(item[0].link) && !item[0].remove) {
-      return item[0].link
-    }
+  if (typeof item === 'string') {
+    return item
+  } else if (item && item.link && ref.isLink(item.link) && !item.remove) {
+    return item.link
   }
 }
 
-function getLatestValue (lookup, key) {
-  var latestTime = 0
-  var latestValue = null
-
-  if (lookup[key]) {
-    for (var author in lookup[key]) {
-      if (Array.isArray(lookup[key][author])) {
-        var value = lookup[key][author][0]
-        var timestamp = lookup[key][author][1]
-        if (timestamp > latestTime) {
-          latestTime = timestamp
-          latestValue = value
-        }
-      }
-    }
+function withDefault (value, defaultValue) {
+  if (typeof defaultValue === 'undefined') {
+    return value
+  } else {
+    return computed([value, defaultValue], fallback)
   }
-
-  return latestValue
 }
 
-function getValueFrom (lookup, key, author) {
-  if (lookup[key] && Array.isArray(lookup[key][author])) {
-    return lookup[key][author][0]
+function fallback (value, defaultValue) {
+  if (value == null) {
+    return defaultValue
+  } else {
+    return value
   }
 }
